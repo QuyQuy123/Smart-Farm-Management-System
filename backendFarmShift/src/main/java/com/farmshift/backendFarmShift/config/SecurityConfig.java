fe25cb7ef61564cae7cbae7f8469c583d1c4c76b
Nguyễn Hữu Hiệp <166497481+ACETech291@users.noreply.github.com>
2026-09-22 22:48:10 +0700

    fix: add new Vercel origin to CORS

--- backendFarmShift/src/main/java/com/farmshift/backendFarmShift/config/SecurityConfig.java
+++ backendFarmShift/src/main/java/com/farmshift/backendFarmShift/config/SecurityConfig.java
@@ -95,8 +95,9 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173", // Local dev
-                "https://admin-ae1mzp943-acet-ech291.vercel.app", // Vercel Production
-                "https://admin-flax-eta-71.vercel.app" // Vercel Aliased
+                "https://admin-ae1mzp943-acet-ech291.vercel.app", // Vercel Production 1
+                "https://admin-flax-eta-71.vercel.app", // Vercel Aliased
+                "https://admin-acet-ech291.vercel.app" // Vercel Main
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
